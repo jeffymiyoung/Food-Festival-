1d31e34619ef4b63652dbd2f8f7a2c8b6c7efb29
Jeffrey Young <jeffreymiyoung@aol.com>
2022-07-18 16:04:55 -0400

    added webpack funtionality

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+// Imports
+const path = require("path");
+const webpack = require("webpack");
+
+// Export for External
+module.exports = {
+    entry: './assets/js/script.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.bundle.js'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        }),
+    ],
+    mode: 'development'
+};
